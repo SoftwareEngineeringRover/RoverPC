624fe6e8bd5a9c968055ff8a3e375668692a23f1
junxinhe <hej77@students.rowan.edu>
2014-12-12 23:30:24 -0500

    Camera works!

--- src/communication/RoverServer.java
+++ src/communication/RoverServer.java
@@ -7,16 +7,15 @@ package communication;
 
 import camera.ImageBuffer;
 import java.awt.image.BufferedImage;
-import java.io.DataOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
-import unpackCommand.UnpackCommand;
+import sun.misc.BASE64Encoder;
 
 /**
  *
@@ -26,16 +25,17 @@ public class RoverServer implements Runnable {
 
     Socket socket = null;
     ServerSocket listener = null;
-    //DataOutputStream out;
     ImageBuffer ib;
     RoverClient client;
+    private PrintWriter out;
+    BASE64Encoder b64enc;
 
     public static void main(String arg[]) {
         try {
             ImageBuffer ib = new ImageBuffer();
             RoverServer server = new RoverServer(ib);
             new Thread(server).start();
-            RoverClient client = new RoverClient("150.250.220.246", ib);
+            RoverClient client = new RoverClient("150.250.221.2", ib);
             client.start();
         } catch (IOException ex) {
             Logger.getLogger(RoverServer.class.getName()).log(Level.SEVERE, null, ex);
@@ -58,15 +58,18 @@ public class RoverServer implements Runnable {
             System.out.println("Server start...");
             socket = listener.accept();
             System.out.println("Client accept");
-
-            //out = new DataOutputStream(socket.getOutputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+            b64enc = new BASE64Encoder();
+            int k = 0;
             while (true) {
-                //out.printf("", ib.getImage(),ib.getImage2());
-                //out.writeObject(ib.getImage());
-                BufferedImage bi=ib.getImage();
-            
-                //System.out.println(bi.toString());
-                ImageIO.write(bi, "JPG", socket.getOutputStream());
+                BufferedImage bi = ib.getImage();
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ImageIO.write(bi, "png", baos);
+                byte[] theBytes = baos.toByteArray();
+                String imageString = b64enc.encode(theBytes);
+                out.println(imageString);
+                out.println("");
+                out.flush();
             }
         } catch (IOException ex) {
             System.out.println("Unable to accept client!");
